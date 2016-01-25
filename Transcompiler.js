8d8824d43365b17d3dc3ccfaf6e203852039d0c4
Joshua Weinstein <jweinst1@berkeley.edu>
2016-01-25 12:21:52 -0800

    made converters safe

--- Transcompiler.js
+++ Transcompiler.js
@@ -26,12 +26,16 @@ var comparops = (function () {
     //fully featured null comparison operator
     comparops.nullop = function (line) {
         var matches = /([^ \(\)]+) =>n/.exec(line);
+        if (matches === null && typeof matches === "object")
+            return line;
         var newstr = matches[1] + " === null && typeof " + matches[1] + " === \"object\"";
         return line.replace(/([^ \(\)]+) =>n/, newstr);
     };
     //array in operator
     comparops.inop = function (line) {
         var matches = /([^ \(\)]+) in (\[[^\[\]]+\]|[^ \(\)]+)/.exec(line);
+        if (matches === null && typeof matches === "object")
+            return line;
         var newline = "(function(" + matches[2] + ", " + matches[1] + ") { for(i=0;i<" + matches[2] + ".length;i++) if(" + matches[2] + "[i]==" + matches[1] + ") return true; return false;})(" + matches[2] + ", " + matches[1] + ")";
         return line.replace(/([^ \(\)]+) in (\[[^\[\]]+\]|[^ \(\)]+)/, newline);
     };
@@ -43,53 +47,71 @@ var mathops = (function () {
     //processes a multiincrement statement
     mathops.increment = function (line) {
         var matches = / (.+?)(\+\++)/.exec(line);
+        if (matches === null && typeof matches === "object")
+            return line;
         var amount = matches[2].length - 1;
         return matches[1] + " += " + amount.toString() + ";";
     };
     //processes a multidecrement statement
     mathops.decrement = function (line) {
         var matches = / (.+?)(\--+)/.exec(line);
+        if (matches === null && typeof matches === "object")
+            return line;
         var amount = matches[2].length - 1;
         return matches[1] + " -= " + amount.toString() + ";";
     };
     //multi power statement
     mathops.powerment = function (line) {
         var matches = / (.+?)(\*\*+)/.exec(line);
+        if (matches === null && typeof matches === "object")
+            return line;
         var amount = matches[2].length - 1;
         return "Math.pow(" + matches[1] + ", " + amount.toString() + ")";
     };
     //deals with the power operator
     mathops.power = function (line) {
         var matches = /([0-9]+) \*\* ([0-9]+)/.exec(line);
+        if (matches === null && typeof matches === "object")
+            return line;
         var newstr = "Math.pow(" + matches[1] + ", " + matches[2] + ")";
         return line.replace(/([0-9]+) \*\* ([0-9]+)/, newstr);
     };
     //max operator
     mathops.max = function (line) {
         var matches = /m\^ ([0-9a-zA-Z, ]+)/.exec(line);
+        if (matches === null && typeof matches === "object")
+            return line;
         var newstr = "Math.max(" + matches[1] + ")";
         return line.replace(/m\^ ([0-9a-zA-Z, ]+)/, newstr);
     };
     //min operator
     mathops.min = function (line) {
         var matches = /m_ ([0-9a-zA-Z, ]+)/.exec(line);
+        if (matches === null && typeof matches === "object")
+            return line;
         var newstr = "Math.min(" + matches[1] + ")";
         return line.replace(/m\^ ([0-9a-zA-Z, ]+)/, newstr);
     };
     //random number operator
     mathops.randnum = function (line) {
         var matches = /([0-9]+) r# ([0-9]+)/.exec(line);
+        if (matches === null && typeof matches === "object")
+            return line;
         var newstr = "Math.floor((Math.random() * " + matches[2] + ") + " + matches[1] + ")";
         return line.replace(/([0-9]+) r# ([0-9]+)/, newstr);
     };
     //random number from 1 to input
     mathops.randupto = function (line) {
         var matches = /([0-9]+)\-r/.exec(line);
+        if (matches === null && typeof matches === "object")
+            return line;
         var newstr = "Math.floor((Math.random() * " + matches[1] + ") + 1)";
         return line.replace(/([0-9]+)\-r/, newstr);
     };
     mathops.squareroot = function (line) {
         var matches = /([0-9]+)\-s/.exec(line);
+        if (matches === null && typeof matches === "object")
+            return line;
         var newstr = "Math.sqrt(" + matches[1] + ")";
         return line.replace(/([0-9]+)\-s/, newstr);
     };
@@ -102,18 +124,24 @@ var stringops = (function () {
     //remove lasts char of string
     stringops.removelast = function (line) {
         var matches = / ([^ \(\)]+|\"[^"]+\") ->l/.exec(line);
+        if (matches === null && typeof matches === "object")
+            return line;
         var newstr = matches[1] + ".slice(0, " + matches[1] + ".length-1)";
         return line.replace(/ ([^ \(\)]+|\"[^"]+\") ->l/, newstr);
     };
     //gets last char of string
     stringops.getlast = function (line) {
         var matches = /l<- ([^ \(\)]+|\"[^"]+\") /.exec(line);
+        if (matches === null && typeof matches === "object")
+            return line;
         var newstr = matches[1] + ".slice(" + matches[1] + ".length-1)";
         return line.replace(/l<- ([^ \(\)]+|\"[^"]+\") /, newstr);
     };
     //removes all instances of substr from a string
     stringops.removeallsubstr = function (line) {
         var matches = /([^ \(\)]+|\"[^"]+\") !> (\"[^"]+\")/.exec(line);
+        if (matches === null && typeof matches === "object")
+            return line;
         var remover = matches[2].slice(1, matches[2].length - 1);
         remover = "/" + remover + "/g";
         var newstr = matches[1] + ".replace(" + remover + ", \"\")";
@@ -122,42 +150,56 @@ var stringops = (function () {
     //gets an array of all numbers in a string
     stringops.getallnumbers = function (line) {
         var matches = / ([^ \(\)]+|\"[^"]+\") ->#/.exec(line);
+        if (matches === null && typeof matches === "object")
+            return line;
         var newstr = matches[1] + ".match(/[0-9]+/g).map(Number)";
         return line.replace(/ ([^ \(\)]+|\"[^"]+\") ->#/, newstr);
     };
     //gets an array of all word sequences
     stringops.getallwords = function (line) {
         var matches = /([^ \(\)]+|\"[^"]+\") ->A/.exec(line);
+        if (matches === null && typeof matches === "object")
+            return line;
         var newstr = matches[1] + ".match(/[a-zA-Z]+/g)";
         return line.replace(/([^ \(\)]+|\"[^"]+\") ->A/, newstr);
     };
     //match operator
     stringops.matchop = function (line) {
         var matches = /([^ \(\)]+|\"[^"]+\") :~ (\/[^\/]+\/|\"[^"]+\")/.exec(line);
+        if (matches === null && typeof matches === "object")
+            return line;
         var newstr = matches[1] + ".match(" + matches[2] + ")";
         return line.replace(/([^ \(\)]+|\"[^"]+\") :~ (\/[^\/]+\/|\"[^"]+\")/, newstr);
     };
     //search operator
     stringops.searchop = function (line) {
         var matches = /([^ \(\)]+|\"[^"]+\") \?~ (\/[^\/]+\/|\"[^"]+\")/.exec(line);
+        if (matches === null && typeof matches === "object")
+            return line;
         var newstr = matches[1] + ".search(" + matches[2] + ")";
         return line.replace(/([^ \(\)]+|\"[^"]+\") \?~ (\/[^\/]+\/|\"[^"]+\")/, newstr);
     };
     //regex test operator regex is always on right side
     stringops.testop = function (line) {
         var matches = /([^ \(\)]+|\"[^"]+\") =~ (\/[^\/]+\/)/.exec(line);
+        if (matches === null && typeof matches === "object")
+            return line;
         var newstr = matches[2] + ".search(" + matches[1] + ")";
         return line.replace(/([^ \(\)]+|\"[^"]+\") =~ (\/[^\/]+\/|\"[^"]+\")/, newstr);
     };
     //returns an array with all matches in a string
     stringops.findallop = function (line) {
         var matches = /([^ \(\)]+|\"[^"]+\") ~> (\/[^\/]+\/)/.exec(line);
+        if (matches === null && typeof matches === "object")
+            return line;
         var newstr = matches[1] + ".match(" + matches[2] + "g)";
         return line.replace(/([^ \(\)]+|\"[^"]+\") ~> (\/[^\/]+\/)/, newstr);
     };
     //returns an array of a string split be a delimeter
     stringops.splitop = function (line) {
         var matches = /([^ \(\)]+|\"[^"]+\") s~ (\/[^\/]+\/|\"[^"]+\")/.exec(line);
+        if (matches === null && typeof matches === "object")
+            return line;
         var newstr = matches[1] + ".split(" + matches[2] + ")";
         return line.replace(/([^ \(\)]+|\"[^"]+\") s~ (\/[^\/]+\/|\"[^"]+\")/, newstr);
     };
@@ -169,36 +211,48 @@ var arrayops = (function () {
     //simulates the length
     arrayops.lengthop = function (line) {
         var matches = /(\[[^\[\]]+\]|[^ \(\)]+)-l/.exec(line);
+        if (matches === null && typeof matches === "object")
+            return line;
         var newstr = matches[1] + ".length";
         return line.replace(/(\[[^\[\]]+\]|[^ \(\)]+)-l/, newstr);
     };
     //push operator implementation
     arrayops.pushop = function (line) {
         var matches = /(\[[^\[\]]+\]|[^ \(\)]+) <p (.+)/.exec(line);
+        if (matches === null && typeof matches === "object")
+            return line;
         var newstr = matches[1] + ".push(" + matches[2] + ")";
         return line.replace(/(\[[^\[\]]+\]|[^ \(\)]+) <p (.+)/, newstr);
     };
     //allows the first occurence of an element in an array to be deleted
     arrayops.pullop = function (line) {
         var matches = /([^ \(\)]+) p> (.+)/.exec(line);
+        if (matches === null && typeof matches === "object")
+            return line;
         var newstr = "delete " + matches[1] + "[" + matches[1] + ".indexof(" + matches[2] + ")]";
         return line.replace(/([^ \(\)]+) p> (.+)/, newstr);
     };
     //operator for creating array copy
     arrayops.copyop = function (line) {
         var matches = /(\[[^\[\]]+\]|[^ \(\)]+)-c/.exec(line);
+        if (matches === null && typeof matches === "object")
+            return line;
         var newstr = matches[1] + ".slice()";
         return line.replace(/(\[[^\[\]]+\]|[^ \(\)]+)-c/, newstr);
     };
     //operator for array max
     arrayops.arraymax = function (line) {
         var matches = /m\^ (\[[^\[\]]+\]|[^ \(\)]+)/.exec(line);
+        if (matches === null && typeof matches === "object")
+            return line;
         var newstr = "Math.max.apply(Math, " + matches[1] + ")";
         return line.replace(/m\^ (\[[^\[\]]+\]|[^ \(\)]+)/, newstr);
     };
     //operator for array min
     arrayops.arraymin = function (line) {
         var matches = /m_ (\[[^\[\]]+\]|[^ \(\)]+)/.exec(line);
+        if (matches === null && typeof matches === "object")
+            return line;
         var newstr = "Math.min.apply(Math, " + matches[1] + ")";
         return line.replace(/m_ (\[[^\[\]]+\]|[^ \(\)]+)/, newstr);
     };
@@ -206,11 +260,15 @@ var arrayops = (function () {
     //gives a literal num range
     arrayops.numrangeop = function (line) {
         var matches = /\[([0-9]+)\]#/.exec(line);
+        if (matches === null && typeof matches === "object")
+            return line;
         var numarr = "(function(n){ var lst = []; for(i=0;i<n;i++) lst.push(i); return lst})(" + matches[1] + ")";
         return line.replace(/\[([0-9]+)\]#/, numarr);
     };
     arrayops.repeatlstop = function (line) {
         var matches = /\[([0-9]+), ([^ \(\)])\]R/.exec(line);
+        if (matches === null && typeof matches === "object")
+            return line;
         var reparr = "(function(n){ var lst = []; for(i=0;i<n;i++) lst.push(" + matches[2] + "); return lst})(" + matches[1] + ")";
         return line.replace(/\[([0-9]+), ([^ \(\)])\]R/, reparr);
     };
--- Transcompiler.ts
+++ Transcompiler.ts
@@ -22,12 +22,14 @@ class comparops {
     //fully featured null comparison operator
     public static nullop(line:string) {
         var matches = /([^ \(\)]+) =>n/.exec(line);
+        if(matches === null && typeof matches === "object") return line;
         var newstr = matches[1] + " === null && typeof " + matches[1] + " === \"object\"";
         return line.replace(/([^ \(\)]+) =>n/, newstr);
     }
     //array in operator
     public static inop(line:string) {
         var matches = /([^ \(\)]+) in (\[[^\[\]]+\]|[^ \(\)]+)/.exec(line);
+        if(matches === null && typeof matches === "object") return line;
         var newline = "(function(" + matches[2] + ", " + matches[1] + ") { for(i=0;i<" + matches[2] + ".length;i++) if(" + matches[2] + "[i]==" + matches[1] + ") return true; return false;})(" + matches[2] + ", " +matches[1] + ")";
         return line.replace(/([^ \(\)]+) in (\[[^\[\]]+\]|[^ \(\)]+)/, newline);
     }
@@ -37,54 +39,63 @@ class mathops {
     //processes a multiincrement statement
     public static increment(line:string) {
         var matches = / (.+?)(\+\++)/.exec(line);
+        if(matches === null && typeof matches === "object") return line;
         var amount = matches[2].length-1;
         return matches[1] + " += " + amount.toString() + ";";
     }
     //processes a multidecrement statement
     public static decrement(line:string) {
         var matches = / (.+?)(\--+)/.exec(line);
+        if(matches === null && typeof matches === "object") return line;
         var amount = matches[2].length-1;
         return matches[1] + " -= " + amount.toString() + ";";
     }
     //multi power statement
     public static powerment(line:string) {
         var matches = / (.+?)(\*\*+)/.exec(line);
+        if(matches === null && typeof matches === "object") return line;
         var amount = matches[2].length-1;
         return "Math.pow(" + matches[1] + ", " + amount.toString() + ")";
     }
     //deals with the power operator
     public static power(line:string) {
         var matches = /([0-9]+) \*\* ([0-9]+)/.exec(line);
+        if(matches === null && typeof matches === "object") return line;
         var newstr = "Math.pow(" + matches[1] + ", " + matches[2] + ")";
         return line.replace(/([0-9]+) \*\* ([0-9]+)/, newstr);
     }
     //max operator
     public static max(line:string) {
         var matches = /m\^ ([0-9a-zA-Z, ]+)/.exec(line);
+        if(matches === null && typeof matches === "object") return line;
         var newstr = "Math.max(" + matches[1] + ")";
         return line.replace(/m\^ ([0-9a-zA-Z, ]+)/, newstr);
     }
     //min operator
     public static min(line:string) {
         var matches = /m_ ([0-9a-zA-Z, ]+)/.exec(line);
+        if(matches === null && typeof matches === "object") return line;
         var newstr = "Math.min(" + matches[1] + ")";
         return line.replace(/m\^ ([0-9a-zA-Z, ]+)/, newstr);
     }
     //random number operator
     public static randnum(line:string) {
         var matches = /([0-9]+) r# ([0-9]+)/.exec(line);
+        if(matches === null && typeof matches === "object") return line;
         var newstr = "Math.floor((Math.random() * " + matches[2] + ") + " + matches[1] + ")";
         return line.replace(/([0-9]+) r# ([0-9]+)/, newstr);
     }
     //random number from 1 to input
     public static randupto(line:string) {
         var matches = /([0-9]+)\-r/.exec(line);
+        if(matches === null && typeof matches === "object") return line;
         var newstr = "Math.floor((Math.random() * " + matches[1] + ") + 1)";
         return line.replace(/([0-9]+)\-r/, newstr);
     }
 
     public static squareroot(line:string) {
         var matches = /([0-9]+)\-s/.exec(line);
+        if(matches === null && typeof matches === "object") return line;
         var newstr = "Math.sqrt(" + matches[1] + ")";
         return line.replace(/([0-9]+)\-s/, newstr);
     }
@@ -94,18 +105,21 @@ class stringops {
     //remove lasts char of string
     public static removelast(line:string) {
         var matches = / ([^ \(\)]+|\"[^"]+\") ->l/.exec(line);
+        if(matches === null && typeof matches === "object") return line;
         var newstr = matches[1] + ".slice(0, " + matches[1] + ".length-1)";
         return line.replace(/ ([^ \(\)]+|\"[^"]+\") ->l/, newstr);
     }
     //gets last char of string
     public static getlast(line:string) {
         var matches = /l<- ([^ \(\)]+|\"[^"]+\") /.exec(line);
+        if(matches === null && typeof matches === "object") return line;
         var newstr = matches[1] + ".slice(" + matches[1] + ".length-1)";
         return line.replace(/l<- ([^ \(\)]+|\"[^"]+\") /, newstr);
     }
     //removes all instances of substr from a string
     public static removeallsubstr(line:string) {
         var matches = /([^ \(\)]+|\"[^"]+\") !> (\"[^"]+\")/.exec(line);
+        if(matches === null && typeof matches === "object") return line;
         var remover = matches[2].slice(1, matches[2].length-1);
         remover = "/" + remover + "/g";
         var newstr = matches[1] + ".replace(" + remover + ", \"\")";
@@ -114,42 +128,49 @@ class stringops {
     //gets an array of all numbers in a string
     public static getallnumbers(line:string) {
         var matches = / ([^ \(\)]+|\"[^"]+\") ->#/.exec(line);
+        if(matches === null && typeof matches === "object") return line;
         var newstr = matches[1] + ".match(/[0-9]+/g).map(Number)";
         return line.replace(/ ([^ \(\)]+|\"[^"]+\") ->#/, newstr);
     }
     //gets an array of all word sequences
     public static getallwords(line:string) {
         var matches = /([^ \(\)]+|\"[^"]+\") ->A/.exec(line);
+        if(matches === null && typeof matches === "object") return line;
         var newstr = matches[1] + ".match(/[a-zA-Z]+/g)";
         return line.replace(/([^ \(\)]+|\"[^"]+\") ->A/, newstr);
     }
     //match operator
     public static matchop(line:string) {
         var matches = /([^ \(\)]+|\"[^"]+\") :~ (\/[^\/]+\/|\"[^"]+\")/.exec(line);
+        if(matches === null && typeof matches === "object") return line;
         var newstr = matches[1] + ".match(" + matches[2] + ")";
         return line.replace(/([^ \(\)]+|\"[^"]+\") :~ (\/[^\/]+\/|\"[^"]+\")/, newstr);
     }
     //search operator
     public static searchop(line:string) {
         var matches = /([^ \(\)]+|\"[^"]+\") \?~ (\/[^\/]+\/|\"[^"]+\")/.exec(line);
+        if(matches === null && typeof matches === "object") return line;
         var newstr = matches[1] + ".search(" + matches[2] + ")";
         return line.replace(/([^ \(\)]+|\"[^"]+\") \?~ (\/[^\/]+\/|\"[^"]+\")/, newstr);
     }
     //regex test operator regex is always on right side
     public static testop(line:string) {
         var matches = /([^ \(\)]+|\"[^"]+\") =~ (\/[^\/]+\/)/.exec(line);
+        if(matches === null && typeof matches === "object") return line;
         var newstr = matches[2] + ".search(" + matches[1] + ")";
         return line.replace(/([^ \(\)]+|\"[^"]+\") =~ (\/[^\/]+\/|\"[^"]+\")/, newstr);
     }
     //returns an array with all matches in a string
     public static findallop(line:string) {
         var matches = /([^ \(\)]+|\"[^"]+\") ~> (\/[^\/]+\/)/.exec(line);
+        if(matches === null && typeof matches === "object") return line;
         var newstr = matches[1] + ".match(" + matches[2] + "g)";
         return line.replace(/([^ \(\)]+|\"[^"]+\") ~> (\/[^\/]+\/)/, newstr);
     }
     //returns an array of a string split be a delimeter
     public static splitop(line:string) {
         var matches = /([^ \(\)]+|\"[^"]+\") s~ (\/[^\/]+\/|\"[^"]+\")/.exec(line);
+        if(matches === null && typeof matches === "object") return line;
         var newstr = matches[1] + ".split(" + matches[2] + ")";
         return line.replace(/([^ \(\)]+|\"[^"]+\") s~ (\/[^\/]+\/|\"[^"]+\")/, newstr);
     }
@@ -160,36 +181,42 @@ class arrayops {
     //simulates the length
     public static lengthop(line:string) {
         var matches = /(\[[^\[\]]+\]|[^ \(\)]+)-l/.exec(line);
+        if(matches === null && typeof matches === "object") return line;
         var newstr = matches[1] + ".length";
         return line.replace(/(\[[^\[\]]+\]|[^ \(\)]+)-l/, newstr);
     }
     //push operator implementation
     public static pushop(line:string) {
         var matches = /(\[[^\[\]]+\]|[^ \(\)]+) <p (.+)/.exec(line);
+        if(matches === null && typeof matches === "object") return line;
         var newstr = matches[1] + ".push(" + matches[2] + ")";
         return line.replace(/(\[[^\[\]]+\]|[^ \(\)]+) <p (.+)/, newstr);
     }
     //allows the first occurence of an element in an array to be deleted
     public static pullop(line:string) {
         var matches = /([^ \(\)]+) p> (.+)/.exec(line);
+        if(matches === null && typeof matches === "object") return line;
         var newstr = "delete " + matches[1] + "[" + matches[1] + ".indexof(" + matches[2] + ")]";
         return line.replace(/([^ \(\)]+) p> (.+)/, newstr);
     }
     //operator for creating array copy
     public static copyop(line:string) {
         var matches = /(\[[^\[\]]+\]|[^ \(\)]+)-c/.exec(line);
+        if(matches === null && typeof matches === "object") return line;
         var newstr = matches[1] + ".slice()";
         return line.replace(/(\[[^\[\]]+\]|[^ \(\)]+)-c/, newstr);
     }
     //operator for array max
     public static arraymax(line:string) {
         var matches = /m\^ (\[[^\[\]]+\]|[^ \(\)]+)/.exec(line);
+        if(matches === null && typeof matches === "object") return line;
         var newstr = "Math.max.apply(Math, " + matches[1] + ")";
         return line.replace(/m\^ (\[[^\[\]]+\]|[^ \(\)]+)/, newstr);
     }
     //operator for array min
     public static arraymin(line:string) {
         var matches = /m_ (\[[^\[\]]+\]|[^ \(\)]+)/.exec(line);
+        if(matches === null && typeof matches === "object") return line;
         var newstr = "Math.min.apply(Math, " + matches[1] + ")";
         return line.replace(/m_ (\[[^\[\]]+\]|[^ \(\)]+)/, newstr);
     }
@@ -197,12 +224,14 @@ class arrayops {
     //gives a literal num range
     public static numrangeop(line:string) {
         var matches = /\[([0-9]+)\]#/.exec(line);
+        if(matches === null && typeof matches === "object") return line;
         var numarr = "(function(n){ var lst = []; for(i=0;i<n;i++) lst.push(i); return lst})(" +matches[1]+ ")";
         return line.replace(/\[([0-9]+)\]#/, numarr);
     }
 
     public static repeatlstop(line:string) {
         var matches = /\[([0-9]+), ([^ \(\)])\]R/.exec(line);
+        if(matches === null && typeof matches === "object") return line;
         var reparr = "(function(n){ var lst = []; for(i=0;i<n;i++) lst.push(" +matches[2]+ "); return lst})(" +matches[1]+ ")";
         return line.replace(/\[([0-9]+), ([^ \(\)])\]R/, reparr);
     }
